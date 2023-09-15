4cb6d93d2c877d93e2f8d56fe0787608e80e76e9
wasin pipattungsakul <wasin.pas@hotmail.com>
2023-09-15 21:55:19 +0930

    test two files

--- func.py
+++ func.py
@@ -3,42 +3,7 @@ import json
 import logging
 from fdk import response
 
-# from .custom_types import ObjectCreateEventPayload
-
-from typing import TypedDict
-
-
-class Extensions(TypedDict):
-    compartmentId: str
-
-
-class AdditionalDetails(TypedDict):
-    eTag: str
-    namespace: str
-    bucketName: str
-    bucketId: str
-    archivalState: str
-
-
-class Data(TypedDict):
-    compartmentId: str
-    compartmentName: str
-    resourceName: str
-    resourceId: str
-    availabilityDomain: str
-    additionalDetails: AdditionalDetails
-
-
-class ObjectCreateEventPayload(TypedDict):
-    cloudEventsVersion: str
-    eventID: str
-    eventType: str
-    source: str
-    eventTypeVersion: str
-    eventTime: str
-    contentType: str
-    extensions: Extensions
-    data: Data
+from custom_types import ObjectCreateEventPayload
 
 
 def handler(ctx, data: io.BytesIO = None):
