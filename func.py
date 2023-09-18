2712361d40ed24162adcac8837b40cf5db83598f
wasin pipattungsakul <wasin.pas@hotmail.com>
2023-09-18 21:59:36 +0930

    test credential

--- func.py
+++ func.py
@@ -11,8 +11,6 @@ from utils.safe_io import BucketHandler
 def handler(ctx, data: io.BytesIO = None):
     # signer = oci.auth.signers.get_resource_principals_signer()
     config = oci.config.from_file('./.oci/config')
-    # logging.getLogger().info('signer')
-    # logging.getLogger().info(signer)
     name = "World"
     try:
         body: ObjectCreateEventPayload = json.loads(data.getvalue())
@@ -29,12 +27,12 @@ def handler(ctx, data: io.BytesIO = None):
         namespace, 'ocid1.compartment.oc1..aaaaaaaazpz6o5zqrptume5fnorrzsavff6n35dl34xrfdxusbroxhsohvoa')
     logging.getLogger().info(buckets.data)
 
-    objects = bucket_handler.list_objects(
-        namespace, 'workshop-data-lake-client-1')
-    logging.getLogger().info(objects.data)
+    # objects = bucket_handler.list_objects(
+    #     namespace, 'workshop-data-lake-client-1')
+    # logging.getLogger().info(objects.data)
 
     return response.Response(
         ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
+            {"message": buckets.data}),
         headers={"Content-Type": "application/json"}
     )
